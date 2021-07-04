cb9544da447df3db63aec944890f7f45e09158db
Tho Vo <votuongtho@gmail.com>
2021-07-04 16:33:19 +0200

    Add images, add BEM for add boat

--- src/app/components/add-boat/add-boat.component.ts
+++ src/app/components/add-boat/add-boat.component.ts
@@ -1,73 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormArray,
-  FormControl,
-  Validators,
-} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { addBoat } from 'src/app/store/actions/boats.actions';
-import { Boat, CatamaranBoat, VoilierBoat } from 'src/app/store/boats.model';
+import { CatamaranBoat, VoilierBoat } from 'src/app/store/boats.model';
 
 @Component({
-  selector: 'app-add-boat',
-  templateUrl: './add-boat.component.html',
-  styleUrls: ['./add-boat.component.scss'],
+	selector: 'app-add-boat',
+	templateUrl: './add-boat.component.html',
+	styleUrls: ['./add-boat.component.scss'],
 })
-export class AddBoatComponent implements OnInit {
-  addForm: FormGroup;
-  currentStep: number = 0;
-  constructor(
-    private _formBuilder: FormBuilder,
-    private store: Store,
-    private router: Router
-  ) {
-    this.addForm = new FormGroup({
-      type: new FormControl('', Validators.required),
-      longeur: new FormControl(''),
-      largeur: new FormControl(''),
-      tirant_eau: new FormControl(''),
-      equipage: new FormControl(false),
-      annexe: new FormControl(false),
-      foil: new FormControl(false),
-    });
-  }
+export class AddBoatComponent {
+	addForm: FormGroup;
+	currentStep: number = 0;
+	constructor(private store: Store, private router: Router) {
+		this.addForm = new FormGroup({
+			type: new FormControl('', Validators.required),
+			longeur: new FormControl(''),
+			largeur: new FormControl(''),
+			tirant_eau: new FormControl(''),
+			equipage: new FormControl(false),
+			annexe: new FormControl(false),
+			foil: new FormControl(false),
+		});
+	}
 
-  ngOnInit(): void {}
+	addNewBoat(): void {
+		const type: string = this.addForm.get('type')?.value;
+		let boat: any;
+		if (type === 'Voilier') {
+			boat = new VoilierBoat(
+				this.addForm.get('longeur')?.value,
+				this.addForm.get('largeur')?.value,
+				this.addForm.get('tirant_eau')?.value,
+				this.addForm.get('equipage')?.value,
+				this.addForm.get('annexe')?.value,
+				this.addForm.get('foil')?.value
+			);
+		}
+		if (type === 'Catamaran') {
+			boat = new CatamaranBoat(
+				this.addForm.get('longeur')?.value,
+				this.addForm.get('largeur')?.value,
+				this.addForm.get('tirant_eau')?.value,
+				this.addForm.get('equipage')?.value,
+				this.addForm.get('annexe')?.value
+			);
+		}
+		this.store.dispatch(addBoat({ boat }));
+		this.router.navigateByUrl('');
+	}
 
-  addNewBoat(): void {
-    const type: string = this.addForm.get('type')?.value;
-    let boat: any;
-    if (type === 'Voilier') {
-      boat = new VoilierBoat(
-        this.addForm.get('longeur')?.value,
-        this.addForm.get('largeur')?.value,
-        this.addForm.get('tirant_eau')?.value,
-        this.addForm.get('equipage')?.value,
-        this.addForm.get('annexe')?.value,
-        this.addForm.get('foil')?.value
-      );
-    }
-    if (type === 'Catamaran') {
-      boat = new CatamaranBoat(
-        this.addForm.get('longeur')?.value,
-        this.addForm.get('largeur')?.value,
-        this.addForm.get('tirant_eau')?.value,
-        this.addForm.get('equipage')?.value,
-        this.addForm.get('annexe')?.value
-      );
-    }
-    this.store.dispatch(addBoat({ boat }));
-    this.router.navigateByUrl('');
-  }
+	previousStep(): void {
+		this.currentStep -= 1;
+	}
 
-  previousStep(): void {
-    this.currentStep -= 1;
-  }
-
-  nextStep(): void {
-    this.currentStep += 1;
-  }
+	nextStep(): void {
+		this.currentStep += 1;
+	}
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { BoatsState } from 'src/app/store/states/boats.state';
@@ -6,49 +6,48 @@ import { tap } from 'rxjs/operators';
 import { CatamaranBoat, VoilierBoat } from 'src/app/store/boats.model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  boats$: Observable<BoatsState>;
-  boats: Array<CatamaranBoat | VoilierBoat> = [];
-  searchText: string = '';
-  filteredBoats: Array<CatamaranBoat | VoilierBoat> = [];
+export class HomeComponent {
+	boats$: Observable<BoatsState>;
+	boats: Array<CatamaranBoat | VoilierBoat> = [];
+	searchText: string = '';
+	filteredBoats: Array<CatamaranBoat | VoilierBoat> = [];
 
-  constructor(private store: Store<{ boats: BoatsState }>) {
-    this.boats$ = this.store.select('boats').pipe(
-      tap((state) => {
-        this.boats = state.boats;
-        this.updateTableResult();
-      })
-    );
-  }
+	constructor(private store: Store<{ boats: BoatsState }>) {
+		this.boats$ = this.store.select('boats').pipe(
+			tap((state) => {
+				this.boats = state.boats;
+				this.updateTableResult();
+			})
+		);
+	}
 
-  ngOnInit(): void {}
-
-  updateTableResult(): void {
-    this.filteredBoats = [];
-    if (this.searchText === '') {
-      this.filteredBoats = [...this.boats];
-    } else {
-      this.boats.forEach((boat) => {
-        let found = false;
-        const searchText = this.searchText.toLowerCase();
-        Object.entries(boat).forEach(([key, value]) => {
-          if (
-            value.toString().toLowerCase().includes(searchText) ||
-            (['equipage', 'annexe', 'foil'].indexOf(key) > 0 &&
-              ((value && 'oui'.includes(searchText)) ||
-                (!value && 'non'.includes(searchText))))
-          ) {
-            found = true;
-          }
-        });
-        if (found) {
-          this.filteredBoats.push(boat);
-        }
-      });
-    }
-  }
+	updateTableResult(): void {
+		this.filteredBoats = [];
+		if (this.searchText === '') {
+			this.filteredBoats = [...this.boats];
+		} else {
+			const booleanList = ['equipage', 'annexe', 'foil'];
+			this.boats.forEach((boat) => {
+				let found = false;
+				const searchText = this.searchText.toLowerCase();
+				Object.entries(boat).forEach(([key, value]) => {
+					if (
+						value.toString().toLowerCase().includes(searchText) ||
+						(booleanList.indexOf(key) > 0 &&
+							((value && 'oui'.includes(searchText)) ||
+								(!value && 'non'.includes(searchText))))
+					) {
+						found = true;
+					}
+				});
+				if (found) {
+					this.filteredBoats.push(boat);
+				}
+			});
+		}
+	}
 }
